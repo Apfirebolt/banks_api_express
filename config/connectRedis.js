b33ab189a621c355dbe1bddbbc5b5ff69f15a538
Apfirebolt <aspper20@gmail.com>
2024-06-10 19:18:03 +0530

    feat: added redis

---
+++ config/connectRedis.js
@@ -0,0 +1,36 @@
+// Import modules
+import { createClient } from "redis";
+
+// Connect to Redis
+const client = createClient({
+    host: "localhost",
+    port: 6379,
+});
+
+// Handle connection errors
+client.on("error", (err) => {
+  console.error("Redis Client Error:", err);
+});
+
+const redisConnect = async () => {
+  await client.connect();
+  console.log("Connected to Redis");
+};
+
+export { client, redisConnect };
+
+// Simple route to test connection
+// app.get("/", async (req, res) => {
+//   try {
+//     // Set a key-value pair in Redis
+//     await client.set("test_key", "Hello from Express!");
+
+//     // Get the value from Redis
+//     const value = await client.get("test_key");
+
+//     res.send(`Connected to Redis! Value: ${value}`);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error connecting to Redis");
+//   }
+// });
